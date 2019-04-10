25e2f8bc62647f7ca495d7a61931a04081074158
luht <19802021149@139.com>
2019-04-10 20:49:29 +0800

    init

---
+++ src/main/java/com/luht/gitshare/GitshareApplication.java
@@ -0,0 +1,13 @@
+package com.luht.gitshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitshareApplication.class, args);
+    }
+
+}
